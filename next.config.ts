3bea29c9a6235ac94b05130cea3e0ab1f33f63d0
Shawon Mondol Shibu <Shawonmondol72@gmail.com>
2025-06-08 18:34:52 +0600

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    domains: ["www.themealdb.com"], // Add the allowed image domain(s) here
+  },
+};
+
+export default nextConfig;
